8ea26d3e46f2d3e88eccb01bab383a4229d77a3a
tenlent <85924276+tenlent@users.noreply.github.com>
2022-04-11 17:53:28 +0800

    Update tencentscf.js

--- tencentscf.js
+++ tencentscf.js
@@ -56,7 +56,7 @@ const sleep = ms => new Promise(res => setTimeout(res, ms));
           ZipFile: contents_in_base64
         },
         FunctionName: process.env.TENCENT_FUNCTION_NAME,
-        Runtime: "Nodejs12.16",
+        Runtime: "Nodejs16.13",
         MemorySize: MemorySize,
         Timeout: 18000,
         AsyncRunEnable: "true",
